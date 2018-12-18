843cf35d786bbfb31b66266f0af01ab94bd3f8b9
Zachery Faria <zacheryfaria@gmail.com>
2018-12-18 15:05:14 -0800

    Added getnextline, strstr, and makefile fixes

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 15:42:25 by awindham          #+#    #+#             */
-/*   Updated: 2018/12/18 15:04:23 by zfaria           ###   ########.fr       */
+/*   Updated: 2018/12/18 15:05:08 by zfaria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,6 +87,6 @@ void				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
-int					get_next_line(int fd, char **line);
+int					get_next_line(int fd, char **line, size_t size_t);
 
 #endif
