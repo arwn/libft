9d7548c4507754ed84455f9d28a232b98151ea96
zacheryfaria <zacheryfaria@gmail.com>
2019-02-22 15:01:49 -0800

    Added unleaky itoa

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -3,41 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: awindham <awindham@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/01 17:08:55 by awindham          #+#    #+#             */
-/*   Updated: 2018/12/02 17:13:04 by awindham         ###   ########.fr       */
+/*   Created: 2018/12/01 14:25:56 by zfaria            #+#    #+#             */
+/*   Updated: 2019/02/22 14:55:28 by zfaria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-/*
-** todo:
-** 1) remove comments
-*/
-
-char		*ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-	char	*str;
+	char			*res;
+	int				i;
+	unsigned int	num;
 
-	if ((str = (char *)malloc(sizeof(char) * 2)) == 0)
-		return (NULL);
-	if (n == -2147483648)
-		return (ft_strcpy(str, "-2147483648"));
-	if (n < 0)
+	res = ft_strnew(13);
+	if (n == 0)
+		return (ft_strdup("0"));
+	if (res)
 	{
-		str[0] = '-';
-		str[1] = '\0';
-		str = ft_strjoin(str, ft_itoa(-n));
+		i = 0;
+		if (n < 0)
+			num = n * -1;
+		else
+			num = n;
+		while (num > 0)
+		{
+			res[i++] = num % 10 + '0';
+			num /= 10;
+		}
+		if (n < 0)
+			res[i++] = '-';
+		res[i] = 0;
+		ft_strrev((unsigned char *)res);
+		return (res);
 	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else if (n < 10 && n >= 0)
-	{
-		str[0] = n + '0';
-		str[1] = '\0';
-	}
-	return (str);
+	return (NULL);
 }
