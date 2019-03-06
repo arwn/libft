fa0365726f187ee9c2af3cc5c7afce689f405786
arwn <windham.aren@protonmail.com>
2019-03-06 12:10:07 -0800

    strvcat

--- src/ft_strvcat.c
+++ src/ft_strvcat.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strvcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: awindham <awindham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/06 12:10:02 by awindham          #+#    #+#             */
+/*   Updated: 2019/03/06 12:10:04 by awindham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <libft.h>
 #include <stdarg.h>
 
@@ -7,7 +19,7 @@ char	*ft_strvcat(char *s, ...)
 	char	*a;
 
 	va_start(vargs, s);
-	while((a = va_arg(vargs, char*)))
+	while ((a = va_arg(vargs, char*)))
 		s = ft_strcat(s, a);
 	return (s);
 }
